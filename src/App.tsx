9e0234309a9916d3a7b019067f9144b6895e7f8a
Roman Unt <untroman@gmail.com>
2025-06-09 11:22:09 +0200

    Add animated card preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { useForm } from '@mantine/form';
 import { TextInput, NumberInput, Button, MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
+import { useState } from 'react';
+import CardPreview from './components/CardPreview';
 
 function App() {
+  const [showBack, setShowBack] = useState(false);
   const form = useForm({
     validateInputOnChange: true,
     validateInputOnBlur: true,
@@ -29,6 +32,13 @@ function App() {
     <MantineProvider>
       <main className="bg-zinc-5 h-screen w-screen pt-10">
         <section className="container mx-auto flex w-[600px] flex-col items-center">
+          <CardPreview
+            cardNumber={String(form.values.cardNumber)}
+            cardholder={form.values.cardholder}
+            expirationDate={String(form.values.expirationDate)}
+            cvv={String(form.values.cvv)}
+            showBack={showBack}
+          />
           <form
             className="flex w-[400px] flex-col items-center gap-4 rounded-lg border border-zinc-200 bg-white p-4"
             onSubmit={form.onSubmit(values => console.log(values))}
@@ -38,12 +48,14 @@ function App() {
               hideControls
               label="Card Number"
               placeholder="1234 5678 9012 3456"
+              onFocus={() => setShowBack(false)}
               {...form.getInputProps('cardNumber')}
             />
             <TextInput
               className="w-full"
               label="Cardholder"
               placeholder="John Doe"
+              onFocus={() => setShowBack(false)}
               {...form.getInputProps('cardholder')}
             />
             <div className="flex w-full gap-4">
@@ -53,6 +65,7 @@ function App() {
                 hideControls
                 label="Expiration Date"
                 placeholder="MM/YY"
+                onFocus={() => setShowBack(false)}
                 {...form.getInputProps('expirationDate')}
               />
               <NumberInput
@@ -61,6 +74,8 @@ function App() {
                 hideControls
                 label="CVV"
                 placeholder="123"
+                onFocus={() => setShowBack(true)}
+                onBlur={() => setShowBack(false)}
                 {...form.getInputProps('cvv')}
               />
             </div>
